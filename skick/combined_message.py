45e1ca4e59429a1eecf7ab0a859205eb604f1aed
awnns <adrian@webyda.se>
2022-09-15 13:29:37 +0200

    Updated the combined message system so that it conforms to the new interface. Replaced most of the comments.

--- skick/combined_message.py
+++ skick/combined_message.py
@@ -1,16 +1,15 @@
 """
-In this module, a messaging system that combine Simple and Rabbit messaging systems is defined.
-If the message is to be sent within servers Simple will be used.
-If the message is to be sent between servers Rabbit will be used.
+In this module, a messaging system that combines the SimpleMessage and
+RabbitMessage messaging systems is defined. They are composed in such a way
+that if a message is to be sent within a shard SimpleMessage will be used,
+but if the message is to be sent between shards, RabbitMessage will be used.
 """
 
 from typing import Any
 import asyncio
 
 from .message_system_interface import MessageSystemInterface, MessageSystemFactory
-
 from .simple_message import SimpleFactory
-
 from .rabbit_message import RabbitFactory
 
 class CombinedMessage(MessageSystemInterface):
@@ -31,14 +30,28 @@ class CombinedMessage(MessageSystemInterface):
 
     async def send(self, address: str, message: dict) -> None:
         """
-        Sends a message using SimpleMessage.send() 
-        if the address is in the SimpleMessage queue.
-        If not it sends a message using RabbitMessage.send().
+        Sends a message using SimpleMessage if the address is in the
+        SimpleMessage queue. If not it sends the message using
+        RabbitMessage.send().
         """
         if address in self.SimpleMessage.queues:
             await self.SimpleMessage.send(address, message)
         else:
             await self.RabbitMessage.send(address, message)
+    
+    async def register_shard(self, address):
+        """
+        This method is concerned with inter-shard communication, so it falls
+        back to the RabbitMessage version of the method.
+        """
+        await self.RabbitMessage.register_shard(address)
+    
+    async def broadcast(self, message: dict) -> None:
+        """
+        This method is used in shard synchronisation, so it falls back to the
+        RabbitMesage version of the method.
+        """
+        await self.RabbitMessage.broadcast(message)
 
 class CombinedFactory(MessageSystemFactory):
     """
